7d3f48d196a5a65a86d59cd71658c199003492fe
BokunoMasayume <allenwalker233333@gmail.com>
2020-04-26 15:10:54 +0800

    i was a total idiot,why  i wrote getFibIndex this simple code and got bug??

--- getFibIndex.js
+++ getFibIndex.js
@@ -3,4 +3,51 @@
 // getFibIndex(4) => -1
 // getFibIndex(21) => 8
 
-function 
+function warpper(){
+    let arr = [1 ,1 ];
+
+    function dimidiate(value){
+        let start = 0;
+        let end = arr.length -1;
+        let mid = ~~((start + end)/2);
+
+        while(start <= end){
+            if(arr[mid] < value ){
+                start = mid +1;
+            }else if(arr[mid] > value ){
+                end = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    return function(value){
+        let curIdx = arr.length - 1;
+
+        if(arr[curIdx] <= value){
+
+        
+            while(arr[curIdx] < value ){
+                curIdx ++;
+                arr[curIdx ] = arr[curIdx -1] + arr[curIdx -2]; 
+            }
+
+            if(value == arr[curIdx]){
+                return curIdx;
+            }else {
+                return -1;
+            }
+
+        }
+
+
+        return dimidiate(value);
+    }
+}
+
+//1 1 2 3 5 8 13 21
+
+module.exports = warpper;
